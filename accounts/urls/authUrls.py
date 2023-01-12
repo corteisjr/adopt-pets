e98c4e1f805af750de5bc8f85d91a85833bb2f48
arcenioCorteis <arcenioCorteis@github.com>
2023-01-12 09:22:40 +0200

    add sqlite for local development

--- accounts/urls/authUrls.py
+++ accounts/urls/authUrls.py
@@ -5,4 +5,5 @@ from accounts.views.registerViews import *
 urlpatterns = [
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
--- pets/settings/development.py
+++ pets/settings/development.py
@@ -12,11 +12,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": os.environ["USER"],
-        "PASSWORD": os.environ["PASSWORD"],
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
